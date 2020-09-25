c44357f1a21980725de4fd9ccf90ad64ffcdba79
Maher El Gamil <maherbusnes@gmail.com>
2020-09-25 23:54:12 +0200

    fix some repositories paths

--- bin/split.sh
+++ bin/split.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
-set -x
+#set -e
+#set -x
 
 CURRENT_BRANCH="master"
 
 function split()
 {
-    SHA1=`./bin/splitsh-lite --prefix=$1`
+    SHA1=`./splitsh-lite --prefix=$1`
     git push $2 "$SHA1:refs/heads/$CURRENT_BRANCH" -f
 }
 
@@ -25,8 +25,8 @@ remote araby-gender git@github.com:buzzylab/araby-gender.git
 remote araby-glyphs git@github.com:buzzylab/araby-glyphs.git
 remote araby-hiero git@github.com:buzzylab/araby-hiero.git
 remote araby-identifier git@github.com:buzzylab/araby-identifier.git
-remote araby-keyswap git@github.com:buzzylab/araby-key-swap.git
-remote araby-maketime git@github.com:buzzylab/araby-maketime.git
+remote araby-key-swap git@github.com:buzzylab/araby-key-swap.git
+remote araby-make-time git@github.com:buzzylab/araby-maketime.git
 remote araby-normalize git@github.com:buzzylab/araby-normalize.git
 remote araby-numbers git@github.com:buzzylab/araby-numbers.git
 remote araby-query git@github.com:buzzylab/araby-query.git
@@ -34,10 +34,10 @@ remote araby-salat git@github.com:buzzylab/araby-salat.git
 remote araby-soundex git@github.com:buzzylab/araby-soundex.git
 remote araby-standard git@github.com:buzzylab/araby-standard.git
 remote araby-stemmer git@github.com:buzzylab/araby-stemmer.git
-remote araby-strtotime git@github.com:buzzylab/araby-str-to-time.git
+remote araby-str-to-time git@github.com:buzzylab/araby-str-to-time.git
 remote araby-summarize git@github.com:buzzylab/araby-summarize.git
 remote araby-transliteration git@github.com:buzzylab/araby-transliteration.git
-remote araby-wordtag git@github.com:buzzylab/araby-word-tag.git
+remote araby-word-tag git@github.com:buzzylab/araby-word-tag.git
 
 split 'src/Araby/Charset' araby-charset
 split 'src/Araby/Compress' araby-compress
